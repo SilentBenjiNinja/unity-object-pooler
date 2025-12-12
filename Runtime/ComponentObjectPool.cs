ee2677becc9c7f95c166127c88e7431c112d95ee
Benji <benjamin.rosenberger@reija.eu>
2025-12-12 11:34:25 +0100

    Add package contents; setup manifest

---
+++ Runtime/ComponentObjectPool.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Core.Creation
+{
+    public class ComponentObjectPool<T> where T : Component
+    {
+        List<T> _pooledObjects = new();
+        Transform _parentTransform;
+
+        T _prefab;
+        string _defaultObjectName;
+
+        public List<T> ActiveObjects => _pooledObjects.Where(x => x.gameObject.activeSelf).ToList();
+
+        public ComponentObjectPool(T prefab, Transform parentTransform, int startAmount, string defaultObjectName = "PooledObject")
+        {
+            _prefab = prefab;
+            _parentTransform = parentTransform;
+            _defaultObjectName = defaultObjectName;
+
+            for (int i = 0; i < startAmount; i++)
+                SpawnNewObject();
+        }
+
+        void SpawnNewObject()
+        {
+            T newObject = Object.Instantiate(_prefab, _parentTransform);
+            newObject.name = _defaultObjectName;
+            FreeObject(newObject);
+            _pooledObjects.Add(newObject);
+        }
+
+        public T Next()
+        {
+            if (_pooledObjects.Count(x => !x.gameObject.activeSelf) == 0)
+                SpawnNewObject();
+
+            return _pooledObjects.FirstOrDefault(x => !x.gameObject.activeSelf);
+        }
+
+        public void FreeObject(T objectToFree) => objectToFree.gameObject.SetActive(false);
+    }
+}
---
+++ Runtime/MB_ObjectPooler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Core.Creation
+{
+    [HideMonoScript]
+    public abstract class MB_ObjectPooler<T> : MonoBehaviour where T : Component
+    {
+        [BoxGroup("Object Pool")]
+        [SerializeField, LabelWidth(100), Range(1, 128)] int _startAmount = 16;
+
+        [BoxGroup("Object Pool")]
+        [SerializeField, LabelWidth(100), AssetsOnly, Required] T _prefab;
+
+        [BoxGroup("Object Pool")]
+        [Tooltip("Leave empty to use this transform")]
+        [SerializeField, LabelWidth(100), LabelText("Parent (optional)")] Transform _parent;
+        protected Transform Parent => _parent == null ? transform : _parent;
+
+        [BoxGroup("Object Pool")]
+        [Tooltip("Leave empty to use prefab's name")]
+        [SerializeField, LabelWidth(100), LabelText("Name (optional)")] string _defaultName;
+        protected string DefaultName =>
+            !string.IsNullOrWhiteSpace(_defaultName) ? _defaultName : _prefab.name + " (Pooled)";
+
+        ComponentObjectPool<T> _pool;
+        protected ComponentObjectPool<T> Pool => _pool;
+
+        public T NextFromPool => _pool.Next();
+
+        protected virtual void Awake() => Load();
+
+        public virtual void Load()
+        {
+            _pool = new(_prefab, Parent, _startAmount, DefaultName);
+        }
+    }
+}
